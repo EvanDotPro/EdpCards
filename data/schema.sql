2699578b4523850a024ad0c3b0826b7ad21bb69f
Evan Coury <me@evancoury.com>
2013-05-20 08:35:09 -0700

    Update schema

--- data/schema.sql
+++ data/schema.sql
@@ -1,3 +1,6 @@
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 CREATE TABLE IF NOT EXISTS `card` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` enum('white','black') NOT NULL,
@@ -18,10 +21,25 @@ CREATE TABLE IF NOT EXISTS `game` (
 CREATE TABLE IF NOT EXISTS `game_card` (
   `game_id` int(11) NOT NULL,
   `card_id` int(11) NOT NULL,
+  `player_id` int(11) DEFAULT NULL,
   `status` enum('available','player','used') NOT NULL DEFAULT 'available',
   UNIQUE KEY `game_id` (`game_id`,`card_id`),
+  KEY `player_id` (`player_id`),
   KEY `card_id` (`card_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `game_round` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `game_id` int(11) NOT NULL,
+  `card_id` int(11) NOT NULL,
+  `judge_id` int(11) NOT NULL,
+  `winner_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `game_id` (`game_id`),
+  KEY `judge_id` (`judge_id`),
+  KEY `card_id` (`card_id`),
+  KEY `winner_id` (`winner_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `player` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -33,22 +51,17 @@ CREATE TABLE IF NOT EXISTS `player` (
   KEY `game_id` (`game_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `player_card` (
-  `player_id` int(11) NOT NULL,
-  `card_id` int(11) NOT NULL,
-  UNIQUE KEY `player_id` (`player_id`,`card_id`),
-  KEY `player_id_2` (`player_id`),
-  KEY `card_id` (`card_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 ALTER TABLE `game_card`
-  ADD CONSTRAINT `game_card_ibfk_2` FOREIGN KEY (`card_id`) REFERENCES `card` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `game_card_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `game_card_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `game_card_ibfk_6` FOREIGN KEY (`card_id`) REFERENCES `card` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `game_card_ibfk_7` FOREIGN KEY (`player_id`) REFERENCES `player` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `game_round`
+  ADD CONSTRAINT `game_round_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `game_round_ibfk_2` FOREIGN KEY (`card_id`) REFERENCES `card` (`id`),
+  ADD CONSTRAINT `game_round_ibfk_3` FOREIGN KEY (`judge_id`) REFERENCES `player` (`id`),
+  ADD CONSTRAINT `game_round_ibfk_4` FOREIGN KEY (`winner_id`) REFERENCES `player` (`id`);
 
 ALTER TABLE `player`
   ADD CONSTRAINT `player_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`) ON DELETE CASCADE;
-
-ALTER TABLE `player_card`
-  ADD CONSTRAINT `player_card_ibfk_2` FOREIGN KEY (`card_id`) REFERENCES `card` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `player_card_ibfk_1` FOREIGN KEY (`player_id`) REFERENCES `player` (`id`) ON DELETE CASCADE;
